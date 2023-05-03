26501c0d5eba2cab2958e8bf7dc34372ecb9e5cd
Ross <rossumj@hotmail.com>
2023-05-03 13:21:24 -0400

    made better links

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,8 +1,8 @@
-import { environment } from "src/environments/environment";
+import { environment } from 'src/environments/environment';
 
 export class AppSettings {
-  static readonly FACEBOOK_URL = 'https://www.facebook.com';
-  static readonly INDEED_URL = 'https://www.indeed.com';
-  static readonly LINKEDIN_URL = 'https://www.linkedin.com';
+  static readonly GITHUB_URL = 'https://github.com/AlphaSpaghett';
+  static readonly INDEED_URL = 'https://profile.indeed.com/p/rosss-bweef8u';
+  static readonly LINKEDIN_URL = 'https://www.linkedin.com/in/ross-stenild-johansen/';
   static readonly TWITTER_URL = 'https://www.twitter.com';
 }
