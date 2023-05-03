c41100f4c5970c67c0c435c614ee50126d8fdd33
Ross Stenild-Johansen <johansenr@uncommongiving.com>
2023-05-03 12:48:25 -0400

    initilization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AppSettings } from './app.settings';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'LinkMe';
+
+  AppSettings = AppSettings;
+
+  constructor(
+  ) {}
+
 }
---
+++ src/app/app.settings.ts
@@ -0,0 +1,8 @@
+import { environment } from "src/environments/environment";
+
+export class AppSettings {
+  static readonly FACEBOOK_URL = 'https://www.facebook.com';
+  static readonly INDEED_URL = 'https://www.indeed.com';
+  static readonly LINKEDIN_URL = 'https://www.linkedin.com';
+  static readonly TWITTER_URL = 'https://www.twitter.com';
+}
